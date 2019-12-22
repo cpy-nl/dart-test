fff196ea1c3c17faef41c2786b8673273b6fca7c
cris <cris.pijper@gmail.com>
2019-12-22 12:28:42 +0100

    Added a reusable component that accepts input

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,14 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
+
 import 'src/hero.dart';
+import 'src/hero_component.dart';
 import 'src/mock_heroes.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: ['app_component.css'],
-  directives: [coreDirectives, formDirectives],
+  directives: [coreDirectives, HeroComponent],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
