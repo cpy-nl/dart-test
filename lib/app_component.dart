e7aae80a64843ec601a3845751ff766cfbb245af
cpy-nl <cris.pijper@gmail.com>
2019-12-23 20:30:40 +0100

    Issues making router available

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,35 +1,25 @@
-import 'dart:async';
-
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 
-import 'src/hero.dart';
-import 'src/hero_component.dart';
 import 'src/hero_service.dart';
+import 'src/routes.dart';
 
 @Component(
   selector: 'my-app',
-  templateUrl: 'app_component.html',
-  styleUrls: ['app_component.css'],
-  directives: [coreDirectives, HeroComponent],
+template: '''
+  <h1>{{title}}</h1>
+  <nav>
+    <a [routerLink]="RoutePaths.dashboard.toUrl()"
+       [routerLinkActive]="'active'">Dashboard</a>
+    <a [routerLink]="RoutePaths.heroes.toUrl()"
+       [routerLinkActive]="'active'">Heroes</a>
+  </nav>
+  <router-outlet [routes]="Routes.all"></router-outlet>
+''',
+  directives: [routerDirectives],
   providers: [ClassProvider(HeroService)],
+  exports: [RoutePaths, Routes],
 )
-
-class AppComponent implements OnInit {
+class AppComponent {
   final title = 'Tour of Heroes';
-  final HeroService _heroService;
-
-  List<Hero> heroes;
-  Hero selected;
-  
-  AppComponent(this._heroService);
-  
-
-  Future<void> _getHeroes() async {
-    heroes = await _heroService.getAll();
-    // asynchronous heroes getter
-  }
-
-  void ngOnInit() => _getHeroes();
-
-  void onSelect(Hero hero) => selected = hero;
 }
---
+++ lib/src/dashboard_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'my-dashboard',
+  templateUrl: 'dahsboard_component.html',
+  directives: [coreDirectives],
+)
+
+class DashboardComponent {
+  
+}
